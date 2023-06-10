50af900827544b73ac44324996bb55817e99cd29
Polja137 <117524191+Polja137@users.noreply.github.com>
2023-06-10 18:01:09 +0200

    chat development step 1

--- components/Chat.js
+++ components/Chat.js
@@ -1,31 +1,70 @@
-import { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, View } from 'react-native';
+import { KeyboardAvoidingView, Platform} from 'react-native';
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
+
 
 const Chat = ({ route, navigation }) => {
-  const { name, color } = route.params;
-  useEffect(() => {
-    navigation.setOptions({ title: name });
+  const { name } = route.params;
+  const [messages, setMessages] = useState([]);
+  const onSend = (newMessages) => {
+   setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+ }
+ const renderBubble = (props) => {
+  return <Bubble
+    {...props}
+    wrapperStyle={{
+      right: {backgroundColor: "#000"},
+      left: {backgroundColor: "#FFF"}
+    }}
+  />
+}
+
+ useEffect(() => {
+  setMessages([
+    {_id: 1,
+      text: 'Hello developer',
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'React Native',
+        avatar: 'https://placeimg.com/140/140/any',
+      },
+    },
+    {_id: 2,
+      text: 'This is a system message',
+      createdAt: new Date(),
+      system: true,},
+  ]);
 }, []);
 
+
+  useEffect(() => {
+    navigation.setOptions({ title: name })
+  }, []);
+
   return (
-    <View style={[{ backgroundColor: color }, styles.container]}>
-      <Text style={color !== "white" ? [{ color: "white" }, styles.title] : styles.title}>
-        Welcome back {name} !
-      </Text>
-    </View>
-  );
-};
+   <View style={styles.container}>
+   <GiftedChat
+     messages={messages}
+     renderBubble={renderBubble}
+     onSend={messages => onSend(messages)}
+     user={{
+       _id: 1
+     }}
+   />
+   {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+   {Platform.OS === "ios"?<KeyboardAvoidingView behavior="padding" />: null}
+   </View> 
+  )
+
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 30,
-  },
+    flex: 1
+  }
 });
 
 export default Chat;
+
